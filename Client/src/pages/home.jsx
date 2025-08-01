a64464eb7d169a6e959f266ffcae9a85217e7764
Khushi <khussipurohit62@gmail.com>
2025-08-01 13:51:28 +0530

    Added a paragraph

--- Client/src/pages/home.jsx
+++ Client/src/pages/home.jsx
@@ -6,6 +6,7 @@ function HomePage() {
       <h1>Welcome to Campus Kala 🎨</h1>
       <p>Buy and sell unique student-made products within your campus!</p>
       <p>LETS BUILD OUR PROJECT CAMPUS KALA</p>
+      <p> THIS IS AN AMAZING APPLICATION</p>
     </div>
   );
 }
